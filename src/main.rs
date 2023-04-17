da7e7a30b3e190d0e309d38102187077407ec3b5
Shin0kari <kasper13122@mail.ru>
2023-04-17 16:17:12 +0300

    added 4 endpoints

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,29 @@ fn world() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/user")]
+fn get_users() -> &'static str {
+    "user1, user2, user_n...!"
+}
+
+#[post("/user")]
+fn post_users() -> &'static str {
+    "user1"
+}
+
+#[put("/user")]
+fn put_users() -> &'static str {
+    "user1_update"
+}
+
+#[delete("/user")]
+fn delete_users() -> &'static str {
+    "DATA DELETED"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![world])
+    rocket::build()
+        .mount("/hello", routes![world])
+        .mount("/", routes![get_users, post_users, put_users, delete_users])
 }
